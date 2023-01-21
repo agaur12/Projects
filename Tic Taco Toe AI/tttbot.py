6401cdf17722eca63a34db34a3fbbab7333dc74a
agaur12 <102323862+agaur12@users.noreply.github.com>
2023-01-20 19:07:11 -0600

    added win check

--- Tic Taco Toe AI/tttbot.py
+++ Tic Taco Toe AI/tttbot.py
@@ -1,21 +1,42 @@
-player, opponent = x, o
+player, opponent = 'x', 'o'
 
-def is_moves_left(board):
+def is_moves_left(b):
     for i in range(3):
-        for j in rang(3):
-            if board[i][j] == '_':
+        for j in range(3):
+            if b[i][j] == '_':
                 return True
     return False
 
 def evaluate(b):
+    for row in range(3):
+        if (b[row][0] == b[row][1] and b[row][1] == b[row][2]):
+            if b[row][0] == player:
+                return 10
+            elif b[row][0] == opponent:
+                return -10
+ 
+    for column in range(3):
+        if (b[0][column] == b[1][column] and b[1][column] == b[2][column]):
+            if b[0][column] == player:
+                return 10
+            elif b[0][column] == opponent:
+                return -10       
     
+    if (b[0][0] == b[1][1] and b[1][1] == b[2][2]):
+        if b[0][0] == player:
+                return 10
+        elif b[0][0] == opponent:
+            return -10 
+    if (b[0][2] == b[1][1] and b[1][1] == b[2][0]):
+        if b[0][2] == player:
+                return 10
+        elif b[0][2] == opponent:
+            return -10 
 
-
-
-board = [     
+board = [   
     [ '_', '_', '_' ],
     [ '_', '_', '_' ],
-    [ '_', '_', '_' ] ]
+    [ '_', '_', '_' ]] 
 
-is_moves_left(board)
-bestMove = find_best_move(board)
+print(evaluate(board))
+#bestMove = find_best_move(board)
